0afeb8527b320b93debf653603322f4c00f50a22
Maksim <maksim.podolinniy@gmail.com>
2023-01-27 15:17:55 +0200

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from './components/footer/Footer';
 
 import styles from './App.module.scss';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className={styles.App}>
+        <Header />
         <Footer />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import logo from "../../assets/img/header-logo.svg";
+import searchIcon from '../../assets/icons/search.svg';
+
+import styles from "./header.module.scss";
 
 function Header(props) {
    return (
-      <div>
-         
+      <div className={styles.header}>
+         <div className="container">
+            <div className={styles.headerInner}>
+               <a className="logo" href="/">
+                  <img src={logo} alt="Our logo" />
+               </a>
+               <nav className={styles.menu}>
+                  <ul className="menu__list">
+                     <li className="menu__item">
+                        <a className="menu__link" href="/">Как купить</a>
+                     </li>
+                     <li className="menu__item">
+                        <a className="menu__link" href="/">F.A.Q</a>
+                     </li>
+                     <li className="menu__item">
+                        <a className="menu__link" href="/">Контакты</a>
+                     </li>
+                  </ul>
+               </nav>
+               <button className={styles.search}>
+                  <img src={searchIcon} alt="Search" />
+               </button>
+               <div className={styles.user}>
+                  <a className={styles.userReg} href="/">Регистрация</a>
+                  <a className={styles.userLogin} href="/">Войти</a>
+               </div>
+            </div>
+         </div>
       </div>
    );
 }
