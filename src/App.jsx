3af39a25df753393d7246af899958f6359674496
Maksim <maksim.podolinniy@gmail.com>
2023-01-28 23:11:59 +0200

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,61 @@ import Slider from './components/slider/Slider';
 import Footer from './components/footer/Footer';
 
 import styles from './App.module.scss';
+import About from './components/about/About';
 
 function App() {
-  return (
-    <div className={styles.App}>
-        <Header />
-        <main>
-          <div className={styles.banner}>
-            <Slider>
-              <div className={styles.slide}>
-                <div className="container">
-                  <div className={styles.slideInner}>
-                    <div>
-                      <p className={styles.slideTitle}>Книги, которые <br/> вам хочется слушать</p>
-                      <p className={styles.slideText}>
-                        Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
-                      </p>
-                    </div>
-                    <button className={styles.slideBtn}>Выбрать книгу</button>
+   return (
+      <div className={styles.App}>
+         <Header />
+         <main>
+            <div className={styles.banner}>
+               <Slider>
+                  <div className={styles.slide}>
+                     <div className="container">
+                        <div className={styles.slideInner}>
+                           <div>
+                              <p className={styles.slideTitle}>Книги, которые <br /> вам хочется слушать</p>
+                              <p className={styles.slideText}>
+                                 Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
+                              </p>
+                           </div>
+                           <button className={styles.slideBtn}>Выбрать книгу</button>
+                        </div>
+                     </div>
                   </div>
-                </div>
-              </div>
-              <div className={styles.slide}>
-                <div className="container">
-                  <div className={styles.slideInner}>
-                    <div>
-                      <p className={styles.slideTitle}>Книги, которые <br/> вам хочется слушать</p>
-                      <p className={styles.slideText}>
-                        Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
-                      </p>
-                    </div>
-                    <button className={styles.slideBtn}>Выбрать книгу</button>
+                  <div className={styles.slide}>
+                     <div className="container">
+                        <div className={styles.slideInner}>
+                           <div>
+                              <p className={styles.slideTitle}>Книги, которые <br /> вам хочется слушать</p>
+                              <p className={styles.slideText}>
+                                 Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
+                              </p>
+                           </div>
+                           <button className={styles.slideBtn}>Выбрать книгу</button>
+                        </div>
+                     </div>
                   </div>
-                </div>
-              </div>
-              <div className={styles.slide}>
-                <div className="container">
-                  <div className={styles.slideInner}>
-                    <div>
-                      <p className={styles.slideTitle}>Книги, которые <br/> вам хочется слушать</p>
-                      <p className={styles.slideText}>
-                        Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
-                      </p>
-                    </div>
-                    <button className={styles.slideBtn}>Выбрать книгу</button>
+                  <div className={styles.slide}>
+                     <div className="container">
+                        <div className={styles.slideInner}>
+                           <div>
+                              <p className={styles.slideTitle}>Книги, которые <br /> вам хочется слушать</p>
+                              <p className={styles.slideText}>
+                                 Лучшая подборка аудиокниг по психологиии саморазвитию за все годы издательства...
+                              </p>
+                           </div>
+                           <button className={styles.slideBtn}>Выбрать книгу</button>
+                        </div>
+                     </div>
                   </div>
-                </div>
-              </div>
-            </Slider>
-          </div>
-        </main>
-        <Footer />
-    </div>
-  );
+               </Slider>
+            </div>
+            <About />
+         </main>
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
