0db3654d5929c56a2696a446740eb28bb4346ad6
Maksim <maksim.podolinniy@gmail.com>
2023-01-30 21:18:53 +0200

    fix SVG

--- src/components/filmCard/FilmCard.jsx
+++ src/components/filmCard/FilmCard.jsx
@@ -15,7 +15,7 @@ function FilmCard({src, title = "Не твоя Людмила", author = "Лин
          <div className={styles.info}>
             <p className={styles.author}>{author}</p>
             <svg viewBox="0 0 24 21" xmlns="http://www.w3.org/2000/svg">
-               <path d="M2.61091 2.62994C0.463029 4.80319 0.463029 8.32672 2.61091 10.5L12.0001 20L21.3891 10.5C23.537 8.32672 23.537 4.80318 21.3891 2.62994C19.2412 0.456688 15.7588 0.456688 13.6109 2.62994L12.0001 4.25994L10.3891 2.62994C8.2412 0.456688 4.7588 0.456688 2.61091 2.62994Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               <path d="M2.61091 2.62994C0.463029 4.80319 0.463029 8.32672 2.61091 10.5L12.0001 20L21.3891 10.5C23.537 8.32672 23.537 4.80318 21.3891 2.62994C19.2412 0.456688 15.7588 0.456688 13.6109 2.62994L12.0001 4.25994L10.3891 2.62994C8.2412 0.456688 4.7588 0.456688 2.61091 2.62994Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
          </div>
          <h3 className={styles.title}>{title}</h3>
