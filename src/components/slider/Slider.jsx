85621aa79aed49f6081ae1653fccf07dc7404ac7
Dmitrij <dmitry.bermas@gmail.com>
2023-02-07 18:44:11 +0100

    added autoplay to slider and some additional animations

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -5,10 +5,30 @@ import styles from "./slider.module.scss";
 function Slider({ children }) {
    const [slides, setSlides] = useState([]);
    const [offset, setOffset] = useState(0);
+   const [isAutoplay, setIsAutoplay] = useState(true);
 
    const _slideWidth = 100;
    const _maxOffset = (slides.length - 1) * _slideWidth;
 
+   //Autoplay functionallity
+   useEffect(() => {
+      let slideInterval;
+      const _intervalDuration = 1000;
+
+      if (isAutoplay) {
+         slideInterval = setInterval(() => {
+            if (offset === _maxOffset) {
+               setOffset(0)
+            } else {
+               setOffset(offset + _slideWidth)
+            }
+         },_intervalDuration)
+      }
+
+      return () => clearInterval(slideInterval)
+   }, [offset, isAutoplay, _maxOffset]);
+
+   //Setting slides
    useEffect(() => {
       setSlides(
          Children.map(children, child => {
@@ -23,30 +43,32 @@ function Slider({ children }) {
       )
    }, [children])
 
-   useEffect(() => {
-      const timer = setTimeout(() => {
-         if (offset === _maxOffset) {
-            setOffset(0)
-         } else {
-            setOffset(offset + _slideWidth)
-         }
-      }, 5000);
-      return () => clearTimeout(timer)
-   });
-
    const onDotClick = (index) => {
       setOffset(_slideWidth * index)
    }
 
    return (
-      <div className={styles.mainContainer}>
-         <ul className={styles.itemsContainer} style={{transform: `translateX(${-offset}%)`}}>
+      <div 
+         className={styles.mainContainer} 
+         onMouseEnter={() => setIsAutoplay(false)} 
+         onMouseLeave={() => setIsAutoplay(true)}
+      >
+         <ul 
+            className={styles.itemsContainer} 
+            style={{transform: `translateX(${-offset}%)`}}
+         >
             {slides}
          </ul>
          
          <div className="container">
             <div className={styles.dots}>
-               {children.map((_, i) => <button onClick={() => onDotClick(i)} key={i} className={`${styles.dot} ${offset === i * 100 ? styles.active : ""}`}></button>)}
+               {children.map((_, i) => (
+                  <button 
+                     onClick={() => onDotClick(i)} 
+                     key={i} 
+                     className={`${styles.dot} ${offset === i * 100 ? styles.active : ""}`}
+                  ></button>
+               ))}
             </div>
          </div>
       </div>
